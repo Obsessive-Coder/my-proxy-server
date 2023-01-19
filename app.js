2dbe2f109402869d144452be0cb5bcf5a43f3c00
Jared Huffstutler <jaredhuff85@gmail.com>
2023-01-19 06:43:37 -0600

    duhhh

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/jokes/random/:endpoint', (req, res) => {
+app.get('/:endpoint', (req, res) => {
   request(
     { url: `http://ecomm-server-dev-env.eba-nrdkp8bh.us-east-1.elasticbeanstalk.com/api/${req.params.endpoint}` },
     (error, response, body) => {
